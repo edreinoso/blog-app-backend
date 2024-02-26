97fce620d200bfe56471c1c8d8cb1a8cef64bcfc
Ed Reinoso <edgardojesus16@gmail.com>
2024-02-26 16:12:08 +0100

    refactor(blog-back): ♻️ adding users information in return from server

--- src/use_cases/__init__.py
+++ src/use_cases/__init__.py
@@ -17,7 +17,7 @@ signup = build_signup(user_repository, password_manager)
 # blogposts CRUD
 add_blogpost = build_add_blogpost(blogpost_repository)
 retrieve_blogpost = build_retrieve_blogpost(blogpost_repository)
-retrieve_all_blogposts = build_retrieve_all_blogposts(blogpost_repository)
+retrieve_all_blogposts = build_retrieve_all_blogposts(blogpost_repository, user_repository)
 edit_blogpost = build_edit_blogpost(blogpost_repository)
 remove_blogpost = build_remove_blogpost(blogpost_repository)
 
--- src/use_cases/retrieve_all_blogposts.py
+++ src/use_cases/retrieve_all_blogposts.py
@@ -1,10 +1,19 @@
-from src.data_access.types import IBlogPostRepository
+from src.data_access.models.user import User
+from src.data_access.types import IBlogPostRepository, IUsersRepository
 from src.entities.blogpost import BlogPost
 
 
-def build_retrieve_all_blogposts(blogpost_repository: IBlogPostRepository):
+def build_retrieve_all_blogposts(
+    blogpost_repository: IBlogPostRepository,
+    user_repository: IUsersRepository
+):
 
-    def retrieve_blogpost() -> list[BlogPost]:
-        return blogpost_repository.get_all()
+    def retrieve_blogpost() -> tuple[list[BlogPost], list[User]]:
+        blogs = blogpost_repository.get_all()
+        
+        users = user_repository.get_all_with_ids(
+            [bp.user_id for bp in blogs])
+        
+        return blogs, users
 
     return retrieve_blogpost
